6e9a435184f403d43815d090de694aeb5bd0acdd
nielsen1778 <nielsen1778@gmail.com>
2015-06-19 17:06:59 -0700

    Initial commit

---
+++ app/src/main/java/com/nielsenclark/spotifystreamer/MainActivityFragment.java
@@ -0,0 +1,332 @@
+package com.nielsenclark.spotifystreamer;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Image;
+
+
+public class MainActivityFragment extends Fragment {
+
+    private final String LOG_TAG = MainActivityFragment .class.getSimpleName();
+
+    List<Artist> listOfArtists;
+
+    private EditText etArtistSearch;
+
+
+    private RecyclerView rvArtists;
+    private RecyclerView.Adapter mArtistsAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private String currentArtist;
+
+    public MainActivityFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        if (currentArtist == null) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            currentArtist = prefs.getString(getString(R.string.pref_last_artist_search_key), getString(R.string.pref_last_artist_search_default));
+            //etArtistSearch.setText(currentArtist);
+        }
+
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        etArtistSearch = (EditText) rootView.findViewById(R.id.etArtistSearch);
+        etArtistSearch.setText(currentArtist);
+        etArtistSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count > 3) {
+                    currentArtist = s.toString();
+
+                    Log.d(LOG_TAG, "On Text Changed: " + currentArtist);
+
+                    FetchArtistsTask artistsTask = new FetchArtistsTask();
+                    artistsTask.execute(currentArtist);
+
+                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString(getString(R.string.pref_last_artist_search_key), s.toString());
+                    editor.commit();
+
+                } else if (count == 0) {
+                    Toast.makeText(getActivity(), "Enter to search for an artist. ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                /*
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(getString(R.string.pref_last_artist_search_key), s.toString());
+                editor.commit();
+                */
+            }
+        });
+        etArtistSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etArtistSearch.setText("");
+            }
+        });
+
+
+        rvArtists = (RecyclerView) rootView.findViewById(R.id.rvArtists);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        rvArtists.setLayoutManager(mLayoutManager);
+
+        mArtistsAdapter = new MyAdapter(listOfArtists);
+        rvArtists.setAdapter(mArtistsAdapter);
+
+        final GestureDetector mGestureDetector = new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+        });
+
+
+        rvArtists.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+
+                if (child != null && mGestureDetector.onTouchEvent(motionEvent)) {
+
+
+                    String spotifyID = listOfArtists.get(recyclerView.getChildPosition(child)).id;
+
+                    Intent topTenTracks = new Intent(getActivity(), TopTracksActivity.class)
+                            .putExtra("SpotifyID", spotifyID);
+                    getActivity().startActivity(topTenTracks);
+
+                    return true;
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+            }
+        });
+
+
+
+
+        return  rootView;
+
+    }
+
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("CurrentArtist", currentArtist);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+
+            currentArtist = savedInstanceState.getString("CurrentArtist");
+            etArtistSearch.setText(currentArtist);
+            //FetchArtistsTask artistsTask = new FetchArtistsTask();
+            //artistsTask.execute(currentArtist);
+        }
+    }
+
+    private void updateArtists() {
+        Log.d(LOG_TAG, "Update Artists");
+        FetchArtistsTask artistsTask = new FetchArtistsTask();
+        artistsTask.execute(currentArtist);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "On Start");
+        updateArtists();
+    }
+
+    public class FetchArtistsTask extends AsyncTask<String, Void, List<Artist>> {
+
+        private final String LOG_TAG = FetchArtistsTask .class.getSimpleName();
+
+
+        @Override
+        protected List<Artist> doInBackground(String... params) {
+
+            if (params.length == 0) {
+                return null;
+            }
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+
+
+            ArtistsPager results = spotify.searchArtists(params[0]);
+
+            listOfArtists = results.artists.items;
+            for(Artist element : listOfArtists){
+                String name = element.name;
+                Log.d(LOG_TAG, "Name" + name);
+            }
+
+
+            if (listOfArtists.size() > 0) {
+                return listOfArtists;
+            } else {
+                return null;
+            }
+
+
+
+        }
+
+        @Override
+        protected void onPostExecute(List<Artist>  result) {
+            if (result != null) {
+                mArtistsAdapter = new MyAdapter(listOfArtists);
+                rvArtists.setAdapter(mArtistsAdapter);
+            } else {
+                Toast.makeText(getActivity(), "Artist not found. ", Toast.LENGTH_SHORT).show();
+
+                listOfArtists.clear();
+                mArtistsAdapter = new MyAdapter(listOfArtists);
+                rvArtists.setAdapter(mArtistsAdapter);
+
+            }
+
+        }
+
+
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        List<Artist> artists;
+
+        MyAdapter(List<Artist> artists){
+            this.artists = artists;
+        }
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ImageView ivArtistPhoto;
+            public TextView tvArtistName;
+
+            public ViewHolder(View v) {
+                super(v);
+                ivArtistPhoto = (ImageView) v.findViewById(R.id.ivArtistPhoto);
+                tvArtistName = (TextView) v.findViewById(R.id.tvArtistName);
+            }
+        }
+
+        public void add(int position, Artist artist) {
+            listOfArtists.add(position, artist);
+            notifyItemInserted(position);
+        }
+
+        public void remove(Artist artist) {
+            int position = listOfArtists.indexOf(artist);
+            listOfArtists.remove(position);
+            notifyItemRemoved(position);
+        }
+
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.artist_line_item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            Artist atemp = listOfArtists.get(position);
+            if (atemp != null) {
+                List<Image> images = listOfArtists.get(position).images;
+
+                if (images != null && images.size() > 0) {
+                    String thumbnailUrl = listOfArtists.get(position).images.get(2).url;
+                    holder.ivArtistPhoto.setImageBitmap(null);
+                    if (thumbnailUrl != null) {
+                        Picasso.with(getActivity()).load(listOfArtists.get(position).images.get(2).url).into(holder.ivArtistPhoto);
+                    }
+                }
+            }
+
+            holder.tvArtistName.setText(listOfArtists.get(position).name);
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            if (listOfArtists != null) {
+                return listOfArtists.size();
+            } else {
+                return 0;
+            }
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/nielsenclark/spotifystreamer/TopTracksActivityFragment.java
@@ -0,0 +1,266 @@
+package com.nielsenclark.spotifystreamer;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Image;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.Tracks;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TopTracksActivityFragment extends Fragment {
+
+    private final String LOG_TAG = MainActivityFragment .class.getSimpleName();
+
+    List<Track> listOfArtistsTopTenTracks;
+
+
+    private RecyclerView rvArtistsTopTenTracks;
+    private RecyclerView.Adapter mArtistsTopTenTracksAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Artist artist;
+    String spotifyID;
+
+    public TopTracksActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_top_tracks, container, false);
+
+        spotifyID = getActivity().getIntent().getExtras().getString("SpotifyID");
+
+
+        rvArtistsTopTenTracks = (RecyclerView) rootView.findViewById(R.id.rvArtistsTopTenTracks);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        rvArtistsTopTenTracks.setLayoutManager(mLayoutManager);
+
+        mArtistsTopTenTracksAdapter = new MyAdapter(listOfArtistsTopTenTracks);
+        rvArtistsTopTenTracks.setAdapter(mArtistsTopTenTracksAdapter);
+
+        final GestureDetector mGestureDetector = new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+        });
+
+        rvArtistsTopTenTracks.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+
+                if (child != null && mGestureDetector.onTouchEvent(motionEvent)) {
+
+
+                //    String spotifyID = listOfArtists.get(recyclerView.getChildPosition(child)).id;
+
+                //    Toast.makeText(getActivity(), "The Item Clicked is: " + recyclerView.getChildPosition(child), Toast.LENGTH_SHORT).show();
+
+                //    Intent playerIntent = new Intent(getActivity(), PlayerActivity.class)
+                //            .putExtra("SpotifyID", spotifyID);
+                //    getActivity().startActivity(playerIntent);
+
+                    return true;
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+            }
+        });
+
+
+        return  rootView;
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("SpotifyID", spotifyID);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+
+            spotifyID = savedInstanceState.getString("SpotifyID");
+            FetchTracksTask tracksTask = new FetchTracksTask();
+            tracksTask.execute(spotifyID);
+        }
+    }
+
+    private void updateTracks() {
+        FetchTracksTask tracksTask = new FetchTracksTask();
+        tracksTask.execute(spotifyID);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateTracks();
+    }
+
+    public class FetchTracksTask extends AsyncTask<String, Void, List<Track>> {
+
+        private final String LOG_TAG = FetchTracksTask .class.getSimpleName();
+
+
+        @Override
+        protected List<Track> doInBackground(String... params) {
+
+            if (params.length == 0) {
+                return null;
+            }
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+
+
+            HashMap<String,Object> queryString = new HashMap<>();
+            queryString.put(SpotifyService.COUNTRY, Locale.getDefault().getCountry());
+
+            Tracks results = spotify.getArtistTopTrack(params[0], queryString);
+
+            listOfArtistsTopTenTracks = results.tracks;
+            for(Track element : listOfArtistsTopTenTracks){
+                String name = element.name;
+                Log.d(LOG_TAG, "Name" + name);
+            }
+
+            return listOfArtistsTopTenTracks;
+        }
+
+        @Override
+        protected void onPostExecute(List<Track>  result) {
+            if (result != null) {
+                mArtistsTopTenTracksAdapter = new MyAdapter(listOfArtistsTopTenTracks);
+                rvArtistsTopTenTracks.setAdapter(mArtistsTopTenTracksAdapter);
+            } else {
+                Toast.makeText(getActivity(), "Tracks not found. ", Toast.LENGTH_SHORT).show();
+
+                listOfArtistsTopTenTracks.clear();
+                mArtistsTopTenTracksAdapter = new MyAdapter(listOfArtistsTopTenTracks);
+                rvArtistsTopTenTracks.setAdapter(mArtistsTopTenTracksAdapter);
+
+            }
+
+        }
+
+
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        List<Track> tracks;
+
+        MyAdapter(List<Track> tracks){
+            this.tracks = tracks;
+        }
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public ImageView ivAlbumPhoto;
+            public TextView tvAlbum;
+            public TextView tvTrackName;
+
+            public ViewHolder(View v) {
+                super(v);
+                ivAlbumPhoto = (ImageView) v.findViewById(R.id.ivAlbumPhoto);
+                tvAlbum = (TextView) v.findViewById(R.id.tvAlbum);
+                tvTrackName = (TextView) v.findViewById(R.id.tvTrackName);
+            }
+        }
+
+        public void add(int position, Track track) {
+            listOfArtistsTopTenTracks.add(position, track);
+            notifyItemInserted(position);
+        }
+
+        public void remove(Track track) {
+            int position = listOfArtistsTopTenTracks.indexOf(track);
+            listOfArtistsTopTenTracks.remove(position);
+            notifyItemRemoved(position);
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.track_line_item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            Track atemp = listOfArtistsTopTenTracks.get(position);
+            if (atemp != null) {
+                List<Image> images = listOfArtistsTopTenTracks.get(position).album.images;
+
+                if (images != null && images.size() > 0) {
+                    String thumbnailUrl = listOfArtistsTopTenTracks.get(position).album.images.get(2).url;
+                    holder.ivAlbumPhoto.setImageBitmap(null);
+                    if (thumbnailUrl != null) {
+                        Picasso.with(getActivity()).load(listOfArtistsTopTenTracks.get(position).album.images.get(2).url).into(holder.ivAlbumPhoto);
+                    }
+                }
+
+            }
+            holder.tvAlbum.setText(listOfArtistsTopTenTracks.get(position).album.name);
+            holder.tvTrackName.setText(listOfArtistsTopTenTracks.get(position).name);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            if (listOfArtistsTopTenTracks != null) {
+                return listOfArtistsTopTenTracks.size();
+            } else {
+                return 0;
+            }
+
+        }
+
+    }
+
+}
